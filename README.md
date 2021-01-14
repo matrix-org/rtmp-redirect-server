14082c5efdde1cb21979c5d8cfc34892a060c954
David Baker <dave@matrix.org>
2021-01-14 16:54:27 +0000

    Here it is

---
+++ README.md
@@ -0,0 +1,4 @@
+rtmp-redirect-server
+====================
+
+A tiny server to redirect rtmp streams to the right place for fosdem
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rtmp-redirect-server",
+    version="0.0.1",
+    author="matrix.org",
+    author_email="admin@matrix.org",
+    description="Tiny server for redirecting RTMP streams for FOSDEM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matrix.org/rtmp-redirect-server",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires='flask',
+)
